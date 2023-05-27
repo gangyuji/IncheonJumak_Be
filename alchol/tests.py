6e41a780e5a0094720c01bc9b2190e51b353821a
gangyuji <gangyoyul9@gmail.com>
2023-05-27 17:17:03 +0900

    🎨  테스트 코드 작성 해보는 중

--- alchol/tests.py
+++ alchol/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from user.models import User
 
 # Create your tests here.
+class ReviewCreateTest(APITestCase):
+    def setUp(self):
+        self.user_data = {'email': 'a.t.alli99u@gmail.com', 'password': 'Dbelddl9$'}
+        self.review_data = {'title': 'uno', 'content': 'unittest'}
+        self.user = User.objects.create_user('')
--- review/tests.py
+++ review/tests.py
@@ -4,9 +4,29 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from user.models import User
 
-# Create your tests here.
+
+
 class ReviewCreateTest(APITestCase):
+    #     self.access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+    @classmethod    
+    def setUpTestData(cls):
+        cls.user_data = {'email': 'unity@ty.ty', 'password': 'universe'}
+        cls.review_data = {'title': 'yunityday', 'content': 'unittest'}
+        cls.user = User.objects.create_user('unity@ty.ty', 'universe')
+        
     def setUp(self):
-        self.user_data = {'email': 'a.t.alli99u@gmail.com', 'password': 'Dbelddl9$'}
-        self.review_data = {'title': 'uno', 'content': 'unittest'}
-        self.user = User.objects.create_user('')
+        self.url = reverse('')
+        
+    def test_fail_if_not_logged_in(self):
+        url = reverse("review_view")
+        response = self.client.post(url, self.review_data)
+        self.assertEqual(response.status_code, 401)
+        
+    # def test_create_review(self):
+    #     response = self.client.post(
+    #         path=reverse("review_view"),
+    #         data=self.review_data,
+    #         HTTP_AUTHORIZATION=f"Bearer {self.access_token}"
+    #     )
+    #     self.assertEqual(response.data["message"], "글 작성 완료-옷!")
+        
